55147063ea53c1134ed79e0ea3fb5a2fd028ec2a
pei <phantares11@gmail.com>
2024-08-23 12:21:16 +0800

    Modify cosine decay min to 0.5

--- src/utils/lr_scheduler.py
+++ src/utils/lr_scheduler.py
@@ -16,6 +16,6 @@ def get_scheduler_with_warmup(
             return current_step / max(1, warmup_steps)
 
         progress = (current_step - warmup_steps) / max(1, total_steps - warmup_steps)
-        return max(0.0, 0.5 * (1.0 + np.cos(np.pi * float(cycles) * 2.0 * progress)))
+        return max(0.5, 0.5 * (1.0 + np.cos(np.pi * float(cycles) * 2.0 * progress)))
 
     return LambdaLR(optimizer, cosine_decay, last_epoch)
