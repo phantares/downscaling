5329763aa337d7a4d0aef2a69523c7438c14fc70
pei <phantares11@gmail.com>
2025-04-01 14:56:43 +0800

    [feat] Add SSIM loss

--- src/models/loss_functions/loss_type.py
+++ src/models/loss_functions/loss_type.py
@@ -3,9 +3,13 @@ from enum import Enum
 from .weightedMSE import WeightedMSE
 from .CRPS import CRPS
 from .SPL import SPL
+from .SSIM import SSIM
+from .MSE_SSIM import MSE_SSIM
 
 
 class LossType(Enum):
     weightedMSE = WeightedMSE
     CRPS = CRPS
     SPL = SPL
+    SSIM = SSIM
+    MSE_SSIM = MSE_SSIM
