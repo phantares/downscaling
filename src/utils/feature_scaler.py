2496a0997e7141fbaa1fa62384e5d4fe743e8976
pei <phantares11@gmail.com>
2025-06-06 14:56:34 +0800

    [feat] Add linearizer

--- src/utils/feature_scaler.py
+++ src/utils/feature_scaler.py
@@ -8,6 +8,7 @@ class ZNormalizer:
     def __init__(
         self, data: torch.Tensor, file_path: str, array_slice: list, suffix: str = ""
     ) -> None:
+
         self.data = data
         data_shape = data.shape
         data_index = tuple([*array_slice, None, None])
@@ -28,41 +29,59 @@ class ZNormalizer:
 
 
 class RobustScaler:
-    def __init__(
-        self, data: torch.Tensor, file_path: str, array_slice: list, suffix: str = ""
-    ) -> None:
-
-        data_shape = data.shape
+    def __init__(self, file_path: str, array_slice: list, suffix: str = "") -> None:
         data_index = tuple([*array_slice, None, None])
 
         self.med = torch.from_numpy(
             np.load(str(Path(file_path, f"med{suffix}.npy")))[data_index]
-        ).expand(data_shape)
-
+        )
         self.iqr = torch.from_numpy(
             np.load(str(Path(file_path, f"iqr{suffix}.npy")))[data_index]
-        ).expand(data_shape)
+        )
 
-    def standardize(self, data) -> torch.Tensor:
-        return (data - self.med) / self.iqr
+    def _expand_array(self, data_shape):
+        med = self.med.expand(data_shape)
+        iqr = self.iqr.expand(data_shape)
 
-    def inverse(self, data) -> torch.Tensor:
-        return data * self.iqr + self.med
+        return med, iqr
+
+    def standardize(self, data: torch.Tensor) -> torch.Tensor:
+        med, iqr = self._expand_array(data.shape)
+
+        return (data - med) / iqr
+
+    def inverse(self, data: torch.Tensor) -> torch.Tensor:
+        med, iqr = self._expand_array(data.shape)
+
+        return data * iqr + med
+
+
+class Linearizer:
+    def __init__(self, max_val, min_val=0) -> None:
+        self.scale = max_val - min_val
+        self.min = min_val
+
+    def standardize(self, data: torch.Tensor) -> torch.Tensor:
+        return (data - self.min) / self.scale
+
+    def inverse(self, data: torch.Tensor) -> torch.Tensor:
+        return data * self.scale + self.min
 
 
 class LogNormalizer:
-    def __init__(self, eps: float = 1e5) -> None:
+    def __init__(self, eps: float = 1e-5) -> None:
         self.eps = eps
 
     def standardize(self, data: torch.Tensor) -> torch.Tensor:
-        return torch.log(data * self.eps + 1)
+        return torch.log(data / self.eps + 1)
 
     def inverse(self, data: torch.Tensor) -> torch.Tensor:
-        return (torch.exp(data) - 1) / self.eps
+        return (torch.exp(data) - 1) * self.eps
 
 
 class ScalingMethod(Enum):
     log_norm = LogNormalizer
+    linear = Linearizer
 
 
 def set_scaling(scaling_configs):
